84aeb987385fbc18c117371df04a0c46692af852
heloint <majerdaniel93@gmail.com>
2023-04-07 21:37:25 +0200

    writing forms still suck, adding react-hook-form to the game

--- app/app/login/page.tsx
+++ app/app/login/page.tsx
@@ -1,75 +1,91 @@
 'use client'
 
-import { SyntheticEvent, useState } from 'react';
+import { useForm, SubmitHandler, UseFormRegister, Path } from "react-hook-form";
+
+interface LoginFormInputs {
+    username: string
+    password: string
+}
+
+type InputProps = {
+   label: string;
+   type: string;
+   id: string;
+   fieldName: Path<LoginFormInputs>;
+   register: UseFormRegister<LoginFormInputs>;
+   placeholder: string;
+   required: boolean;
+}
 
 export default function Login() {
+    const onSubmit: SubmitHandler<LoginFormInputs> = (data) => console.log(data);
+    const { register, handleSubmit, formState: { errors } } = useForm<LoginFormInputs>();
 
-    const usernameField: HTMLInputElement = document.querySelector("#login-username") as HTMLInputElement;
-    const passwordField: HTMLInputElement = document.querySelector("#login-username") as HTMLInputElement;
+    // const submit = (e: SyntheticEvent) => {
+    //     e.preventDefault();
 
-    const [username, setUsername] = useState(usernameField?.value);
-    const [password, setPassword] = useState(passwordField?.value);
+    //     fetch('http://localhost:8000/login', {
+    //         method: "POST",
+    //         headers: {
+    //             "Access-Control-Allow-Origin": "*",
+    //             "Content-Type": "text/plain"
+    //         },
+    //         credentials: 'include',
+    //         body: JSON.stringify({
+    //             username: username,
+    //             password: password
+    //         })
+    //     });
+    // }
 
-    const submit = (e: SyntheticEvent) => {
-        e.preventDefault();
-
-        fetch('http://localhost:8000/login', {
-            method: "POST",
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": "text/plain"
-            },
-            credentials: 'include',
-            body: JSON.stringify( {
-              username: username,
-              password: password
-            })
-        });
-    }
-
-  return (
-    <div id="Login" className="container">
-      <div className="row justify-content-center my-5">
-        <div className="col-xl-6 col-lg-6 col-md-10 d-grid gap-2">
-            <h3>Login</h3>
-            <form onSubmit={submit} className="d-grid gap-3">
-                <div className="form-group">
-                  <label htmlFor="login-username" className="form-label">
-                    Username
-                  </label>
-                  <input
-                    type="text"
+    const Input = ({ label, type, id, fieldName, register, placeholder, required }: InputProps) => (
+        <>
+            <div className="form-group">
+                <label>{label}</label>
+                <input id={id}
+                    type={type}
                     className="form-control"
-                    id="login-username"
-                    name="username"
-                    placeholder="Enter username"
-                    onChange={e => setUsername(e.target.value)}
-                  />
-                </div>
+                    placeholder={placeholder}
+                    {...register(fieldName, { required })} />
+            </div>
+        </>
+    );
 
-                <div className="form-group">
-                  <label htmlFor="login-pass" className="form-label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="login-pass"
-                    name="password"
-                    placeholder="Password"
-                    onChange={e => setPassword(e.target.value)}
-                  />
-                </div>
-                <div className="form-group">
-                    <button type="submit" className="btn btn-primary">
-                        Login
-                    </button>
-                </div>
-                
-            </form>
+    return (
+        <div id="Login" className="container">
+            <div className="row justify-content-center my-5">
+                <div className="col-xl-6 col-lg-6 col-md-10 d-grid gap-2">
+                    <h3>Login</h3>
+                    <form onSubmit={handleSubmit(onSubmit)} className="d-grid gap-3">
+                        <Input id="login-username"
+                            label="Username"
+                            type="text"
+                            fieldName="username"
+                            register={register}
+                            placeholder="Enter username"
+                            required
+                        />
+                        {errors.username && <p className="text-danger">Username is required</p>}
 
+                        <Input id="login-pass"
+                            label="Password"
+                            type="password"
+                            fieldName="password"
+                            register={register}
+                            placeholder="Enter password"
+                            required
+                        />
+                        {errors.password && <p className="text-danger">Username is required</p>}
+
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">
+                                Login
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
